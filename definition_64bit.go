f39310ad6f48fec30174278613183d9a8080000a
holmanskih <evg.holmanskih@gmail.com>
2023-05-22 16:51:20 +0300

    Revert "remove x64 definitions"
    This reverts commit 7bf30da7b88ed38d1dfeef586920501050ab3caa.

--- definition_64bit.go
+++ definition_64bit.go
@@ -1,4 +1,4 @@
-// +build 386,arm,mipsle,mips,wasm
+// +build !386 !arm !mipsle !mips !wasm
 
 package backoff
 
@@ -8,7 +8,7 @@ import (
     "time"
 )
 
-const maxDuration = float64(math.MaxInt32 - 512)
+const maxDuration = float64(math.MaxInt64 - 512)
 
 // Backoff is a time.Duration counter, starting at Min. After every call to
 // the Duration method the current timing is multiplied by Factor, but it
@@ -17,7 +17,7 @@ const maxDuration = float64(math.MaxInt32 - 512)
 // Backoff is not generally concurrent-safe, but the ForAttempt method can
 // be used concurrently.
 type Backoff struct {
-	attempt uint32
+	attempt uint64
 	// Factor is the multiplying factor for each increment step
 	Factor float64
 	// Jitter eases contention by randomizing backoff steps
@@ -29,16 +29,16 @@ type Backoff struct {
 // Duration returns the duration for the current attempt before incrementing
 // the attempt counter. See ForAttempt.
 func (b *Backoff) Duration() time.Duration {
-	d := b.ForAttempt(float64(atomic.AddUint32(&b.attempt, 1) - 1))
+	d := b.ForAttempt(float64(atomic.AddUint64(&b.attempt, 1) - 1))
 	return d
 }
 
 // Reset restarts the current attempt counter at zero.
 func (b *Backoff) Reset() {
-	atomic.StoreUint32(&b.attempt, 0)
+	atomic.StoreUint64(&b.attempt, 0)
 }
 
 // Attempt returns the current attempt counter value.
 func (b *Backoff) Attempt() float64 {
-	return float64(atomic.LoadUint32(&b.attempt))
+	return float64(atomic.LoadUint64(&b.attempt))
 }
